22900466eb9ab8262beaa6cf8137db108fa3ccdc
vitquaybarkingg <leanhviet2005@gmail.com>
2025-10-22 22:13:32 +0700

    Tao class category

--- src/main/java/com/mycompany/appquanlychitieu/model/Category.java
+++ src/main/java/com/mycompany/appquanlychitieu/model/Category.java
@@ -9,5 +9,51 @@ package com.mycompany.appquanlychitieu.model;
  * @author Duck
  */
 public class Category {
-    
+     private int id;
+    private String name;
+    private String description;
+
+    public Category() {}
+
+    public Category(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public void createCategory(String name, String des) {
+        this.name = name;
+        this.description = des;
+        System.out.println("Category created: " + name);
+    }
+
+    public void updateDetails(String new_name, String new_des) {
+        this.name = new_name;
+        this.description = new_des;
+        System.out.println("Category updated: " + new_name);
+    }
+
+    public double getTotalSpent(String startDate, String endDate) {
+        // Giả lập tính tổng chi tiêu trong khoảng thời gian
+        System.out.println("Calculating total spent from " + startDate + " to " + endDate);
+        return 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
