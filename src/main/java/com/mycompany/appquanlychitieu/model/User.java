5344135bb0b1a87c24bd97058b5bf442ba12792f
minh_tien <162453950+tubi22@users.noreply.github.com>
2025-11-28 00:02:47 +0700

    User ver1

--- src/main/java/com/mycompany/appquanlychitieu/model/User.java
+++ src/main/java/com/mycompany/appquanlychitieu/model/User.java
@@ -6,8 +6,39 @@ package com.mycompany.appquanlychitieu.model;
 
 /**
  *
- * @author Duck
+ * @author tien
  */
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+class Account {}
+class Category {}
+class Debt {}
+class RecurringSchedule {}
+
 public class User {
+    public Long id;
+    public String name;
+    public LocalDateTime createdAt;
+    public LocalDateTime updatedAt;
     
+    public String email;
+    public String passwordHash;
+    public String avatarPath;
+    public String defaultCurrency;
+
+    public List<Account> accounts;
+    public List<Category> categories;
+    public List<Debt> debts;
+    public List<RecurringSchedule> recurringSchedules;
+
+    public User() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.accounts = new ArrayList<>();
+        this.categories = new ArrayList<>();
+        this.debts = new ArrayList<>();
+        this.recurringSchedules = new ArrayList<>();
+    }
 }
