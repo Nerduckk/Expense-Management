08cb31bebe03172f995f18373edbe5610aada4a0
DarkBre <163324273+DarkBre@users.noreply.github.com>
2025-11-28 00:19:41 +0700

    Version 1
    khai báo các thuộc tính dựa trên UML

--- src/main/java/com/mycompany/appquanlychitieu/model/Debt.java
+++ src/main/java/com/mycompany/appquanlychitieu/model/Debt.java
@@ -3,11 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.appquanlychitieu.model;
-
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 /**
  *
- * @author Duck
+ * @author 
  */
+enum DebtType {
+    BORROWING, 
+    LENDING    
+}
+
+enum DebtStatus {
+    ACTIVE,    
+    COMPLETED, 
+    BAD_DEBT   
+}
+class BaseEntity {
+    public Long id;
+    public String name;
+    public LocalDateTime createdAt;
+    public LocalDateTime updatedAt;
+}
+
+class Debt extends BaseEntity {
+    public DebtType type;
+    public String personName;
+    public BigDecimal principalAmount;
+    public Double interestRate;
+    public LocalDate startDate;
+    public LocalDate dueDate;
+    public DebtStatus status;
+}
 public class Debt {
     
 }
