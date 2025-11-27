3083d894a56df53376d3b7b1acf0a9deaa7d10ea
vitquaybarking <108894246+Nerduckk@users.noreply.github.com>
2025-11-27 23:49:24 +0700

    Update code cơ bản ver 2 của class Account

--- src/main/java/com/mycompany/appquanlychitieu/model/Account.java
+++ src/main/java/com/mycompany/appquanlychitieu/model/Account.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.appquanlychitieu.model;
+import java.math.BigDecimal;
 
-/**
- *
- * @author Duck
- */
 public class Account {
-    
+    private Long id;      
+    private String name;   
+    private BigDecimal balance;
+    private String currency;
+
+    public Account(Long id, String name, BigDecimal initialBalance) {
+        this.id = id;
+        this.name = name;
+        this.balance = initialBalance;
+    }
+
+    public void credit(BigDecimal amount) {
+        this.balance = this.balance.add(amount);
+    }
+
+    public void debit(BigDecimal amount) {
+        this.balance = this.balance.subtract(amount);
+    }
+
+    public Long getId() { return id; }
+    public BigDecimal getBalance() { return balance; }
+    public String getName() { return name; }
 }
