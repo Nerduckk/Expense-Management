0bddc240e21ca3ebcea9510dadfbb2c80d545207
DarkBre <163324273+DarkBre@users.noreply.github.com>
2025-11-28 01:35:01 +0700

    Version 2
    Thêm tính đóng gói, getter/setter

--- src/main/java/com/mycompany/appquanlychitieu/model/Category.java
+++ src/main/java/com/mycompany/appquanlychitieu/model/Category.java
@@ -8,9 +8,32 @@ package com.mycompany.appquanlychitieu.model;
  *
  * @author Hoang
  */
+import java.math.BigDecimal;
+
 public class Category extends BaseEntity {
-    public CategoryType type;
-    public String icon;
-    public String color;
-    public BigDecimal budgetLimit;
+    private CategoryType type;
+    private String icon;
+    private String color;
+    private BigDecimal budgetLimit;
+
+    public Category() {}
+
+    public Category(String name, CategoryType type, String icon, String color) {
+        this.setName(name);
+        this.type = type;
+        this.icon = icon;
+        this.color = color;
+    }
+
+    public CategoryType getType() { return type; }
+    public void setType(CategoryType type) { this.type = type; }
+
+    public String getIcon() { return icon; }
+    public void setIcon(String icon) { this.icon = icon; }
+
+    public String getColor() { return color; }
+    public void setColor(String color) { this.color = color; }
+
+    public BigDecimal getBudgetLimit() { return budgetLimit; }
+    public void setBudgetLimit(BigDecimal budgetLimit) { this.budgetLimit = budgetLimit; }
 }
