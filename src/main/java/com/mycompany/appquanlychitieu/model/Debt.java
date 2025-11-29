7a020a7c4e3d626c1016c1403607d44bee822afc
minh_tien <162453950+tubi22@users.noreply.github.com>
2025-11-29 23:25:26 +0700

    Update Debt.java

--- src/main/java/com/mycompany/appquanlychitieu/model/Debt.java
+++ src/main/java/com/mycompany/appquanlychitieu/model/Debt.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.appquanlychitieu.model;
 import java.math.BigDecimal;
