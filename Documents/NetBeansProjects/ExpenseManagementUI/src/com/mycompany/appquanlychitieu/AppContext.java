481f704318a734c69f1be1e9a965de3be66b1a14
vitquaybarkingg <leanhviet2005@gmail.com>
2025-11-30 05:26:57 +0700

    Thêm debt vao appcontext

--- Documents/NetBeansProjects/ExpenseManagementUI/src/com/mycompany/appquanlychitieu/AppContext.java
+++ Documents/NetBeansProjects/ExpenseManagementUI/src/com/mycompany/appquanlychitieu/AppContext.java
@@ -5,10 +5,12 @@ import com.mycompany.appquanlychitieu.service.DataStore;
 import com.mycompany.appquanlychitieu.service.TransactionService;
 import com.mycompany.appquanlychitieu.service.CategoryService;
 import com.mycompany.appquanlychitieu.service.AccountService;
+import com.mycompany.appquanlychitieu.service.DebtService; 
 public class AppContext {
     public static final TransactionService transactionService = new TransactionService();
     public static final CategoryService categoryService = new CategoryService();
     public static final AccountService  accountService  = new AccountService();
+    public static final DebtService     debtService     = new DebtService();
     private static User currentUser;
 
     public static void init() {
