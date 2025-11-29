172d35c135d1fc0d358d185ae041697417a39930
tiendatngn <164172597+tiendatngn@users.noreply.github.com>
2025-11-29 15:45:07 +0700

    Update FileHelper.java

--- src/main/java/com/mycompany/appquanlychitieu/service/FileHelper.java
+++ src/main/java/com/mycompany/appquanlychitieu/service/FileHelper.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.appquanlychitieu.service;
-
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Admin
  */
 public class FileHelper {
-    
+
+    public static <T> void saveToFile(List<T> data, String fileName) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            oos.writeObject(data);
+            System.out.println("Đã lưu thành công: " + fileName);
+        } catch (IOException e) {
+            System.err.println("Lỗi khi lưu file " + fileName + ": " + e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> loadFromFile(String fileName) {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            return new ArrayList<>(); 
+        }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            return (List<T>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Lỗi khi đọc file " + fileName + ": " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
 }
